300bca66a4763c200be3f944757597f0b425e62a
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-23 23:43:09 +0530

    registered movies with admin

--- movie-recommender/movies/admin.py
+++ movie-recommender/movies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from movies.models import Movie, Genre
+from movies.forms import MovieChangeListForm
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Genre)
