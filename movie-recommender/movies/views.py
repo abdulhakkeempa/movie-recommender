c7dc2723aa3e6bed77cbc9357c567c1fb90cc296
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-23 23:43:44 +0530

    some changes

--- movie-recommender/movies/views.py
+++ movie-recommender/movies/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.views import View
+from movies.models import Movie
 
 # Create your views here.
 class Movies(View):
   def get(self, request):
-    return render(request, 'movies/movies.html')
+    movies = Movie.objects.all()
+    print(movies)
+    return render(request, 'movies/movies.html', {'movies': movies})
   
 
 def get_movies(request, pk):
-  return render(request, 'movies/movies-individual.html')
+  movie = Movie.objects.get(pk=pk)
+  return render(request, 'movies/movies-individual.html', {'movie': movie})
