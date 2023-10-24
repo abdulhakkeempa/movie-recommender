db0162602f03f8d833db3cfa4fb4773f11471ad5
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-10-24 22:58:48 +0530

    chore: made class based views for movies operations

--- movie-recommender/movies/views.py
+++ movie-recommender/movies/views.py
@@ -1,15 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
-from movies.models import Movie
+from django.views.generic import ListView
+from movies.models import Movie, UserMovieRating
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
-class Movies(View):
-  def get(self, request):
-    movies = Movie.objects.all()
-    print(movies)
-    return render(request, 'movies/movies.html', {'movies': movies})
+
+class ListMovies(LoginRequiredMixin, ListView):
+  model = Movie
+  context_object_name = 'movies'
+  template_name = 'movies/movies.html'
+
+class MovieDetail(LoginRequiredMixin, View):
+  def get(self, request, pk):
+    movie = Movie.objects.get(pk=pk)
+    return render(request, 'movies/movies-individual.html', {'movie': movie})
   
+  def post(self, request, pk):
+    movie = Movie.objects.get(pk=pk)
+    user = request.user
+    rating = request.POST.get('review')
 
-def get_movies(request, pk):
-  movie = Movie.objects.get(pk=pk)
-  return render(request, 'movies/movies-individual.html', {'movie': movie})
+    if UserMovieRating.objects.filter(user=user, movie=movie).exists():
+      rating = UserMovieRating.objects.get(user=user, movie=movie)
+      rating.rating = rating
+      rating.save()
+      return redirect('movies-individual', pk=pk)
+
+    rating = UserMovieRating(user=user, movie=movie, rating=rating)
+    rating.save()
+    return redirect('movies-individual', pk=pk)
