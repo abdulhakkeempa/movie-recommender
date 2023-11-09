6aad996a52f06f209fcd59b162a2b8eae92fe850
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-09 13:49:36 +0530

    chore: added data for downloading

--- movie-recommender/movies/admin.py
+++ movie-recommender/movies/admin.py
@@ -3,6 +3,11 @@ from movies.models import Movie, Genre, UserMovieRating
 from movies.forms import MovieChangeListForm
 
 # Register your models here.
-admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(UserMovieRating)
+
+
+class MovieAdmin(admin.ModelAdmin):
+  list_filter = ['language']
+
+admin.site.register(Movie, MovieAdmin)
