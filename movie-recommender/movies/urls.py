bc3d3c36571078227cda6156003954c50bdbfb59
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-11 19:22:38 +0530

    ui: added a seperate page for listing all movies with pagination

--- movie-recommender/movies/urls.py
+++ movie-recommender/movies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from movies.views import  ListMovies, MovieDetail
+from movies.views import  ListMovies, MovieDetail, ListAllMovies
 
 urlpatterns = [
     path("", ListMovies.as_view(), name="movies"),
+    path("all", ListAllMovies.as_view(), name="all-movies"),
     path("<int:pk>", MovieDetail.as_view(), name="movies-individual"),
 ]
