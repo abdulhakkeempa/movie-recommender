87c1e9f3d666666e0945d5e5b01a08a099eb7a87
abdulhakkeempa <abdulhakkeempa2002@gmail.com>
2023-11-11 20:29:45 +0530

    fix: index error caused due to unknown movies in training data fixed

--- movie-recommender/movies/recommendations.py
+++ movie-recommender/movies/recommendations.py
@@ -28,18 +28,27 @@ def get_item_based_recommendation(movie_name):
     """
     n_movies_to_reccomend = 10
     movie_list = movies[movies['title'].str.contains(movie_name)]  
-    if len(movie_list):        
+    if len(movie_list):
+        print(movie_list)        
         movie_idx= movie_list.iloc[0]['movieId']
-        movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
-        distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
-        rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
-        recommend_frame = []
-        for val in rec_movie_indices:
-            movie_idx = final_dataset.iloc[val[0]]['movieId']
-            idx = movies[movies['movieId'] == movie_idx].index
-            recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
-        df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
-        return df['Title'].to_list()
+        try:
+          movie_idx = final_dataset[final_dataset['movieId'] == movie_idx].index[0]
+          distances , indices = knn.kneighbors(csr_data[movie_idx],n_neighbors=n_movies_to_reccomend+1)    
+          rec_movie_indices = sorted(list(zip(indices.squeeze().tolist(),distances.squeeze().tolist())),key=lambda x: x[1])[:0:-1]
+          recommend_frame = []
+          for val in rec_movie_indices:
+              movie_idx = final_dataset.iloc[val[0]]['movieId']
+              idx = movies[movies['movieId'] == movie_idx].index
+              recommend_frame.append({'Title':movies.iloc[idx]['title'].values[0],'Distance':val[1]})
+          df = pd.DataFrame(recommend_frame,index=range(1,n_movies_to_reccomend+1))
+          return df['Title'].to_list()
+        
+        except IndexError as e:
+           return [] # if the movie is not in the training data
+        
+        except Exception as e:
+           print("Something went wrong")
+           print(e)
     else:
         return []
 
